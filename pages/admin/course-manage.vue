a2f42c275af9e5d4ed4750c10a123446fe92c849
JetGreen <grishadzyin@gmail.com>
2024-12-13 16:41:24 +0500

    update course for admin

--- pages/admin/course-manage.vue
+++ pages/admin/course-manage.vue
@@ -3,8 +3,9 @@ definePageMeta({
   middleware: ["admin"],
 })
 
-import type { Course } from '~/types/course.interface';
-import type { User } from '~/types/user.interface';
+import { toast } from "vue3-toastify";
+import type { Course } from "~/types/course.interface"
+import type { User } from "~/types/user.interface"
 
 const courseStore = useCourse()
 const authStore = useAuth()
@@ -14,32 +15,11 @@ let { courses } = storeToRefs(courseStore)
 
 let coursesToSelect = ref<any>([])
 let selectedCourseId = ref()
-let selectedCourse = ref<Course>();
-
-let imagesFormData = new FormData()
-let visibleCropperModal = ref(false)
-let dialog = ref(false);
-
-let photoPreview = ref()
-function uploadLogo(file: File) {
-  imagesFormData.set('logo', file, 'logo_' + String(Date.now()) + '.jpg')
-  let reader = new FileReader();
-  reader.onloadend = function () {
-    photoPreview.value = reader.result
-  }
-  reader.readAsDataURL(file)
-  visibleCropperModal.value = false
-}
-
-function deletePreview() {
-  imagesFormData.set('logo', ' ')
-  photoPreview.value = null
-}
-
+let selectedCourse = ref<Course>()
 
 let form = ref({
-  name: '',
-  shortDescription: '',
+  name: "",
+  shortDescription: "",
 })
 let users = ref<User[]>([])
 
@@ -49,7 +29,7 @@ if (courses.value) {
   for (let c of courses.value) {
     coursesToSelect.value.push({
       title: c.name,
-      value: c._id
+      value: c._id,
     })
   }
 }
@@ -73,112 +53,101 @@ watch(selectedCourseId, (newSelectedCourse) => {
   if (courses.value) {
     for (let course of courses.value) {
       if (course._id == newSelectedCourse) {
-        form.value.name = course.name;
-        form.value.shortDescription = course.shortDescription;
-        selectedCourse.value = course;
+        form.value.name = course.name
+        form.value.shortDescription = course.shortDescription
+        selectedCourse.value = course
         return
       }
     }
   }
 })
 
+let loading = ref(false)
+let submitCount = 0;
+async function submit() {
+  submitCount++;
+  if (submitCount > 1 || !authStore.user?._id) return;
+  loading.value = true
+
+  let res = await courseStore.updateCourse(form.value, selectedCourseId.value, authStore.user.roles[0])  
+  if (res.status.value == "success") {
+    loading.value = false;
+    toast("Курс успешно обновлён!", {
+      type: "success"
+    })
+  }
+}
+
 let response = await authStore.getAllUsers()
 
-if (response.status.value == 'success') {
+if (response.status.value == "success") {
   users.value = response.data.value
 }
 
 onMounted(() => {
-  selectedCourseId.value = route.query.id;
+  selectedCourseId.value = route.query.id
 })
 </script>
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" class="mb-4">
+      <v-col cols="12">
         <p class="text-4xl font-semibold">Редактировать курс</p>
-        <BackButton class="mt-4"></BackButton>
+        <BackButton class="mt-4" />
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-select label="выбрать курс" :items="coursesToSelect" variant="outlined" v-model="selectedCourseId">
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row v-if="selectedCourseId">
-      <v-col cols="12" md="8">
-        <v-text-field variant="outlined" label="Название" v-model="form.name"></v-text-field>
-        <v-textarea variant="outlined" label="Описание" v-model="form.shortDescription"></v-textarea>
-      </v-col>
-
-      <v-col cols="12" md="4" class="pb-8">
-        <div @click="visibleCropperModal = true" class="h-100">
-          <label for="uploadLogo">
-            <div class="h-100 flex flex-col align-center justify-center rounded border-opacity-25	border-sm">
-              <v-icon icon="mdi mdi-cloud-upload-outline" class="icon" />
-              <span class="explanation text-center">Загрузить обложку курса</span>
-            </div>
-          </label>
-        </div>
-
-        <v-dialog v-model="dialog" width="auto">
-          <v-card max-width="400" title="Удалить фото">
-            <template v-slot:actions>
-              <v-btn text="нет" @click="dialog = false;"></v-btn>
-              <v-btn text="да" @click="dialog = false; deletePreview()"></v-btn>
-            </template>
-          </v-card>
-        </v-dialog>
-
-        <v-dialog v-model="visibleCropperModal">
-          <v-row class="justify-center">
-            <v-col cols="12" md="8" lg="6">
-              <v-card class="pa-4 rounded-lg">
-                <ImageCropper @uploadImage="uploadLogo" />
-
-                <v-card-actions>
-                  <v-btn @click="visibleCropperModal = false" variant="tonal" size="large" class="btn ml-auto">
-                    закрыть
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-dialog>
-      </v-col>
-      <v-col cols="12">
-        <v-img class="my-2 relative z-0 overflow-visible rounded" :src="photoPreview" width="200" v-if="photoPreview">
-          <v-btn class="absolute -top-7 z-50 btn border" @click="dialog = true"
-            icon="mdi mdi-trash-can-outline"></v-btn>
-        </v-img>
-      </v-col>
-
-      <v-col cols="12">
-        <p class="text-1xl font-semibold">Добавить пользователя в курс</p>
-      </v-col>
-      <v-col cols="12">
-        <v-row v-for="user of users">
-          <v-col class="d-flex align-center">
-            <v-icon icon="mdi-account"></v-icon>
-            {{ user.name }}
-            {{ user.surname }}
-            <v-btn class="ml-6" size="small" @click="addUserToCourse(user._id)"
-              :disabled="isUserInCourse(user._id)">добавить</v-btn>
+      <v-col cols="8">
+        <v-row>
+          <v-col cols="12">
+            <v-select
+              label="выбрать курс"
+              :items="coursesToSelect"
+              variant="outlined"
+              v-model="selectedCourseId"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" v-if="selectedCourseId">
+            <v-row>
+              <v-col cols="12">
+                <v-text-field variant="outlined" label="Название" v-model="form.name"></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea variant="outlined" label="Описание" v-model="form.shortDescription"></v-textarea>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="12" class="flex justify-center">
-        <v-btn size="x-large">Создать</v-btn>
+
+      <v-col cols="4"> фотки </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-col cols="12" class="border" style="border-radius: 36px">
+          <v-col cols="12">
+            <p class="text-2xl font-semibold">Люди в курсе</p>
+          </v-col>
+          <v-col cols="12">
+            <v-row v-for="user of users">
+              <v-col class="d-flex align-center">
+                <v-icon icon="mdi-account"></v-icon>
+                {{ user.name }}
+                {{ user.surname }}
+                <v-btn class="ml-6" size="small" @click="addUserToCourse(user._id)" :disabled="isUserInCourse(user._id)"
+                  >добавить</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-col>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" class="d-flex justify-end">
+        <v-btn class="submit-button" type="text" @click="submit" :loading="loading"
+          >Изменить курс <v-icon>mdi-check</v-icon></v-btn
+        >
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<style>
-.btn {
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, 0);
-}
-</style>
