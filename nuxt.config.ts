997faf1715fb883c165fa915d0f7dd4deb2a4915
JetGreen <grishadzyin@gmail.com>
2024-12-28 10:31:55 +0400

    add tg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   css: ['~/assets/styles/main.scss'],
   devtools: { enabled: true },
+  routeRules: {
+    '/miniapp/**': {
+      ssr: false,
+    }
+  },
   build: {
     transpile: ['vuetify'],
   },
--- pages/miniapp/index.vue
+++ pages/miniapp/index.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  script: [{
+    src: 'https://telegram.org/js/telegram-web-app.js?56',
+  }]
+})
+
+console.log(window.Telegram.initData);
+</script>
 <template>
   <div>
     Неверный miniapp
