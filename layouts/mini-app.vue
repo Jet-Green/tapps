2b4476e33981af86c99acb159f3a7df2aa4c8071
JetGreen <grishadzyin@gmail.com>
2025-01-12 14:50:03 +0500

    add bg color in css

--- layouts/mini-app.vue
+++ layouts/mini-app.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-onMounted(() => {
-  window.Telegram?.WebApp?.ready();
-})
 </script>
 <template>
   <!-- чистый layout для страницы самого mini app -->
-  <div>
+  <div style="background-color: white;">
     <slot />
   </div>
 </template>
