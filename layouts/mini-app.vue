ad14781db1874fa36c3f4034004fcb0e77d8f2da
JetGreen <grishadzyin@gmail.com>
2025-01-12 14:55:08 +0500

    add min-weight 100dvh

--- layouts/mini-app.vue
+++ layouts/mini-app.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <!-- чистый layout для страницы самого mini app -->
-  <div style="background-color: white;">
+  <div style="background-color: white; min-height: 100dvh;">
     <slot />
   </div>
 </template>
